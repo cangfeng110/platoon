2f6f4bc8ef76be15f855f33fb43fd2f972018f2e
zhangshuai <zhangshuai4181@126.com>
2020-01-08 15:20:30 +0800

    version 0.004

---
+++ build/include/base/CMakeFiles/platoon-base.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/igs/zsworkspace/platoon/include/base/Channel.cpp" "/home/igs/zsworkspace/platoon/build/include/base/CMakeFiles/platoon-base.dir/Channel.cpp.o"
+  "/home/igs/zsworkspace/platoon/include/base/CountDownLatch.cpp" "/home/igs/zsworkspace/platoon/build/include/base/CMakeFiles/platoon-base.dir/CountDownLatch.cpp.o"
+  "/home/igs/zsworkspace/platoon/include/base/EventLoop.cpp" "/home/igs/zsworkspace/platoon/build/include/base/CMakeFiles/platoon-base.dir/EventLoop.cpp.o"
+  "/home/igs/zsworkspace/platoon/include/base/FileLogger.cpp" "/home/igs/zsworkspace/platoon/build/include/base/CMakeFiles/platoon-base.dir/FileLogger.cpp.o"
+  "/home/igs/zsworkspace/platoon/include/base/Logging.cpp" "/home/igs/zsworkspace/platoon/build/include/base/CMakeFiles/platoon-base.dir/Logging.cpp.o"
+  "/home/igs/zsworkspace/platoon/include/base/Poller.cpp" "/home/igs/zsworkspace/platoon/build/include/base/CMakeFiles/platoon-base.dir/Poller.cpp.o"
+  "/home/igs/zsworkspace/platoon/include/base/TimerQueue.cpp" "/home/igs/zsworkspace/platoon/build/include/base/CMakeFiles/platoon-base.dir/TimerQueue.cpp.o"
+  "/home/igs/zsworkspace/platoon/include/base/Timestamp.cpp" "/home/igs/zsworkspace/platoon/build/include/base/CMakeFiles/platoon-base.dir/Timestamp.cpp.o"
+  "/home/igs/zsworkspace/platoon/include/base/Utils.cpp" "/home/igs/zsworkspace/platoon/build/include/base/CMakeFiles/platoon-base.dir/Utils.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/modules/common/CMakeFiles/platoon-common.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "libplatoon-common.a"
+)
